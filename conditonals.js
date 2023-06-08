a3e94229dca7f83da1d5c4d8eabf9e5d7deec310
Bernard Gyamfi <bernardogyamfi@gmail.com>
2023-06-08 16:34:13 +0000

    first js commit

---
+++ conditonals.js
@@ -0,0 +1,12 @@
+//  3. Funtion that adds 3 numbers and returns answer
+
+function addNumbers(num1, num2, num3) {
+  var sum = num1 + num2 + num3;
+  return sum;
+}
+
+
+// 4. console log the answer
+
+var results = addNumbers(5, 8, 10);
+console.log(results);
